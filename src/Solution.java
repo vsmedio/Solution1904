64add5932851a514ce5509ffe96677986bc32c67
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 20:56:56 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,78 @@
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/* 
+И еще один адаптер
+Адаптировать Scanner к PersonScanner.
+Классом-адаптером является PersonScannerAdapter.
+В классе адаптере создать приватное финальное поле Scanner fileScanner. Поле инициализировать в конструкторе с одним аргументом типа Scanner.
+
+Данные в файле хранятся в следующем виде:
+Иванов Иван Иванович 31 12 1950
+Петров Петр Петрович 31 12 1957
+
+В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные только одного человека.
+
+
+Требования:
+1. PersonScanner должен быть интерфейсом.
+2. Класс PersonScannerAdapter должен реализовывать интерфейс PersonScanner.
+3. Класс PersonScannerAdapter должен содержать приватное поле fileScanner типа Scanner.
+4. Класс PersonScannerAdapter должен содержать конструктор с параметром Scanner.
+5. Метод close() класса PersonScannerAdapter должен делегировать полномочие такому же методу fileScanner.
+6. Метод read() класса PersonScannerAdapter должен читать строку с файла, парсить её, и возвращать данные только одного человека, в виде объекта класса Person.
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class PersonScannerAdapter {
+
+    }
+}
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class PersonScannerAdapter implements PersonScanner {
+        private final Scanner fileScanner;
+
+        public PersonScannerAdapter(Scanner scanner) {
+            this.fileScanner = scanner;
+        }
+
+        @Override
+        public Person read() throws IOException {
+            Person person = null;
+            if (fileScanner.hasNext()) {
+                String s = fileScanner.nextLine();
+                String[] parts = s.split(" ");
+
+                String firstName = parts[1];
+                String middleName = parts[2];
+                String lastName = parts[0];
+
+                Calendar calendar = new GregorianCalendar(Integer.parseInt(parts[5]),
+                        Integer.parseInt(parts[4])-1,
+                        Integer.parseInt(parts[3]));
+
+                person = new Person(firstName, middleName, lastName, calendar.getTime());
+            }
+            return person;
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+        }
+    }
+}
